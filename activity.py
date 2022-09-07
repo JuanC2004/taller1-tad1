6eabdce84813df6cd06716f85654661f4a6ae04f
JuanC2004 <juanc.quinteroh@autonoma.edu.co>
2022-09-07 15:06:49 -0500

    The main archive was created, and the class actiivity with the lists

---
+++ activity.py
@@ -0,0 +1,14 @@
+'''
+Author: Juan Camilo Quintero Hurtado
+Date: 07/09/22
+Language: python 3.0
+
+'''
+class Activity:
+    def _init_ (self):
+        self.dairy=[]
+        self.clean=[]
+        self.grain=[]    
+
+    print(f"Las listas creadas son dairy, clean y grain")
+    
---
+++ main1.py
@@ -0,0 +1,12 @@
+'''
+Author: Juan Camilo Quintero Hurtado
+Date: 07/09/22
+Language: python 3.0
+
+'''
+
+from activity import Activity
+
+inst_activity= Activity()
+
+
